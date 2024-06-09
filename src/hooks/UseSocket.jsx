05fede7f23f56e162e90960eeebbd6373d405330
walltch <walltch@github.com>
2024-06-09 20:52:15 +0200

    update link

--- src/hooks/UseSocket.jsx
+++ src/hooks/UseSocket.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
 const useSocket = () => {
-  const url = 'http://localhost:4000';
+  const url = 'https://letsplayback.onrender.com';
   const [socket, setSocket] = useState(null);
   const [isConnected, setIsConnected] = useState(false);
 
